14e96ce3e6a5fd3567174210253293effab6f133
FlamingHerb <mbomisol@up.edu.ph>
2025-05-27 00:38:15 +0800

    feat: add hover animation for the view button for specific projects

--- src/routes/sdg-[num]/ProjectTableItem.svelte
+++ src/routes/sdg-[num]/ProjectTableItem.svelte
@@ -33,7 +33,7 @@
 
     </td>
     <td class="py-4">
-        <button class="float-right flex font-dmsans" onclick={view_item}>
+        <button class="float-right flex font-dmsans hover:bg-white hover:text-black p-1 rounded-md transition duration-150" onclick={view_item}>
             View
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                 <path fill-rule="evenodd" d="M8.25 3.75H19.5a.75.75 0 0 1 .75.75v11.25a.75.75 0 0 1-1.5 0V6.31L5.03 20.03a.75.75 0 0 1-1.06-1.06L17.69 5.25H8.25a.75.75 0 0 1 0-1.5Z" clip-rule="evenodd" />
