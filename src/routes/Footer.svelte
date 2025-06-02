7371b2102f6def17da796b09fad896b8d293b290
amabyado <amabyado@up.edu.ph>
2025-06-02 20:57:51 +0800

    feat: mobile responsive footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,10 +1,10 @@
 <nav id="about" class="flex flex-row items-center bottom-0 w-full min-h-[20dvh] bg-black text-white border-t border-white/30 mt-[50px]">
-    <div class="flex flex-row items-center bg-black-500 w-full h-full px-[150px] py-[10px] gap-[20px]">
-        <div class="flex flex-col justify-center w-[50%] gap-[5px]">
+    <div class="flex flex-col sm:flex-row items-center bg-black-500 w-full h-full px-[30px] sm:px-[150px] py-[30px] sm:py-[10px] gap-[20px]">
+        <div class="flex flex-col justify-center w-full sm:w-[50%] gap-[5px]">
             <h1 class="text-xl">Pilipinas In A Nutshell © 2025</h1>
             <p class="text-sm">Pilipinas in a Nutshell (PilipiNuts) 2024 is a data science awareness initiative, presenting the current state of the Philippines in 2024 through visualizations.</p>
         </div>
-        <div class="flex flex-col justify-center items-end w-[50%] gap-[10px] font-medium">
+        <div class="flex flex-col justify-center items-start sm:items-end w-full sm:w-[50%] gap-[10px] font-medium">
             <a href="/"><p>Home</p></a>
             <a href="/#sdgProjects"><p>Projects By SDG</p></a>
             <a href="/bts"><p>Meet The Team</p></a>
