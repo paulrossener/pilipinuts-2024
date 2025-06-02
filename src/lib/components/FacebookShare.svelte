72246907541f553a458a95b301ce557220a58a10
FlamingHerb <mbomisol@up.edu.ph>
2025-06-02 22:18:07 +0800

    fix: base pathing for sharing feature

--- src/lib/components/FacebookShare.svelte
+++ src/lib/components/FacebookShare.svelte
@@ -13,7 +13,7 @@
 <!-- svelte-ignore a11y_consider_explicit_label -->
 <a 
     class="text-inherit font-medium px-4 rounded inline-block transition duration-150"
-    href="https://www.facebook.com/sharer/sharer.php?u={page.url.pathname}/#{encodeURIComponent(encoded_url)}"
+    href="https://www.facebook.com/sharer/sharer.php?u={base}{page.url.pathname}/#{encodeURIComponent(encoded_url)}"
     target="_blank"
 >
     <span class="icon-[uiw--facebook] text-[36px]"></span>
--- src/lib/components/TwitterShare.svelte
+++ src/lib/components/TwitterShare.svelte
@@ -13,7 +13,7 @@
 <!-- svelte-ignore a11y_consider_explicit_label -->
 <a
     class="font-medium px-4 rounded inline-block transition duration-150"
-    href="https://twitter.com/intent/post?text={tweet_text}&url=https://www.wallahi.com{page.url.pathname}/#{encodeURIComponent(encoded_url)}"
+    href="https://twitter.com/intent/post?text={tweet_text}&url={base}{page.url.pathname}/#{encodeURIComponent(encoded_url)}"
     target="_blank"
 >
     <span class="icon-[line-md--twitter-x] text-[36px]"></span>
