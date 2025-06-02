c6715f10b655c057f18fcfbb4965916d2c11a56a
pingu204 <73891290+pingu204@users.noreply.github.com>
2025-06-03 00:08:15 +0800

    chore: update lg accent

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -2,7 +2,7 @@
     <div class="flex flex-col sm:flex-row items-center bg-black-500 w-full h-full px-[30px] sm:px-[150px] py-[30px] sm:py-[10px] gap-[20px]">
         <div class="flex flex-col justify-center w-full sm:w-[50%] gap-[5px]">
             <h1 class="text-xl">Pilipinas In A Nutshell © 2025</h1>
-            <p class="text-sm">Pilipinas in a Nutshell (PilipiNuts) 2024 is a data science awareness initiative, presenting the current state of the Philippines in 2024 through visualizations.</p>
+            <p class="text-sm font-mono">Pilipinas in a Nutshell (PilipiNuts) 2024 is a data science awareness initiative, presenting the current state of the Philippines in 2024 through visualizations.</p>
         </div>
         <div class="flex flex-col justify-center items-start sm:items-end w-full sm:w-[50%] gap-[10px] font-medium">
             <a href="/"><p>Home</p></a>
